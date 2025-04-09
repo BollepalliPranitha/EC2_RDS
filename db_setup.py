aac77dafdeb89736f714da15229503f49f8c2360
Bollepalli Pranitha <bollepallipranitha228@gmail.com>
2025-04-09 17:42:50 -0400

    initial changes for db setup and connecting ec2_9

--- db_setup.py
+++ db_setup.py
@@ -3,32 +3,46 @@ import json
 import yaml
 
 def load_config():
+    """Load configuration from config.yaml file."""
     with open('config.yaml', 'r') as f:
         config = yaml.safe_load(f)
     return config
 
 def connect_to_rds(config, database='master', autocommit=False):
+    """Connect to the RDS instance."""
     driver_path = "/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.10.so.6.1"
     connection_string = f'DRIVER={driver_path};SERVER={config["RDS_HOST"]},{config["DB_PORT"]};DATABASE={database};UID={config["DB_USER"]};PWD={config["DB_PASSWORD"]}'
     return pyodbc.connect(connection_string, autocommit=autocommit)
 
 def create_database(cursor, db_name):
+    """Create the database if it does not exist."""
     cursor.execute(f"IF DB_ID('{db_name}') IS NULL CREATE DATABASE {db_name};")
     #cursor.commit()  # Commit the database creation
 
-def create_tables(cursor):
+def table_exists(cursor, table_name):
+    """Check if a table exists in the database."""
     cursor.execute("""
-        IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Hotels' AND xtype='U')
-        CREATE TABLE Hotels (
+        SELECT COUNT(*) 
+        FROM INFORMATION_SCHEMA.TABLES 
+        WHERE TABLE_NAME = ?
+    """, (table_name,))
+    return cursor.fetchone()[0] > 0
+
+def create_hotel_table(cursor):
+    """Create the Hotels table if it does not exist."""
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS Hotels (
             HotelID INT PRIMARY KEY IDENTITY(1,1),
             HotelName VARCHAR(255) NOT NULL,
             Location VARCHAR(255) NOT NULL,
             TotalRooms INT NOT NULL
         );
     """)
+
+def create_room_table(cursor):
+    """Create the Rooms table if it does not exist."""
     cursor.execute("""
-        IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Rooms' AND xtype='U')
-        CREATE TABLE Rooms (
+        CREATE TABLE IF NOT EXISTS Rooms (
             RoomID INT PRIMARY KEY IDENTITY(1,1),
             HotelID INT,
             RoomNumber INT NOT NULL,
@@ -38,9 +52,11 @@ def create_tables(cursor):
             CONSTRAINT UC_Room UNIQUE(HotelID, RoomNumber)
         );
     """)
+
+def create_guest_table(cursor):
+    """Create the Guests table if it does not exist."""
     cursor.execute("""
-        IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Guests' AND xtype='U')
-        CREATE TABLE Guests (
+        CREATE TABLE IF NOT EXISTS Guests (
             GuestID INT PRIMARY KEY IDENTITY(1,1),
             FirstName VARCHAR(255) NOT NULL,
             LastName VARCHAR(255) NOT NULL,
@@ -48,9 +64,11 @@ def create_tables(cursor):
             Phone VARCHAR(20)
         );
     """)
+
+def create_booking_table(cursor):
+    """Create the Bookings table if it does not exist."""
     cursor.execute("""
-        IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Bookings' AND xtype='U')
-        CREATE TABLE Bookings (
+        CREATE TABLE IF NOT EXISTS Bookings (
             BookingID INT PRIMARY KEY IDENTITY(1,1),
             GuestID INT,
             RoomID INT,
@@ -62,13 +80,25 @@ def create_tables(cursor):
         );
     """)
 
+def create_tables(cursor):
+    """Create all necessary tables if they do not exist."""
+    if not table_exists(cursor, 'Hotels'):
+        create_hotel_table(cursor)
+    if not table_exists(cursor, 'Rooms'):
+        create_room_table(cursor)
+    if not table_exists(cursor, 'Guests'):
+        create_guest_table(cursor)
+    if not table_exists(cursor, 'Bookings'):
+        create_booking_table(cursor)
+
 def insert_data(cursor, data):
+    """Insert data into the database."""
     for hotel in data['Hotels']:
         cursor.execute("""
             IF NOT EXISTS (SELECT 1 FROM Hotels WHERE HotelName = ?)
             INSERT INTO Hotels (HotelName, Location, TotalRooms)
             VALUES (?, ?, ?)
-        """, (hotel['HotelName'], hotel['HotelName'], hotel['Location'], hotel['TotalRooms']))
+        """, (hotel['HotelName'], hotel['Location'], hotel['TotalRooms']))
     
     for room in data['Rooms']:
         cursor.execute("""
@@ -82,7 +112,7 @@ def insert_data(cursor, data):
             IF NOT EXISTS (SELECT 1 FROM Guests WHERE Email = ?)
             INSERT INTO Guests (FirstName, LastName, Email, Phone)
             VALUES (?, ?, ?, ?)
-        """, (guest['Email'], guest['FirstName'], guest['LastName'], guest['Email'], guest['Phone']))
+        """, (guest['FirstName'], guest['LastName'], guest['Email'], guest['Phone']))
 
     for booking in data['Bookings']:
         cursor.execute("""
@@ -91,6 +121,7 @@ def insert_data(cursor, data):
         """, (booking['GuestID'], booking['RoomID'], booking['CheckInDate'], booking['CheckOutDate'], booking['TotalCost']))
 
 def main():
+    """Main function to set up the database and insert data."""
     config = load_config()
     with open('datasets/data.json', 'r') as f:
         data = json.load(f)
@@ -105,8 +136,8 @@ def main():
     connection = connect_to_rds(config, database=config['DB_NAME'])
     cursor = connection.cursor()
     
-    create_tables(cursor)
-    insert_data(cursor, data)
+    create_tables(cursor)  # Create tables if they don't exist
+    insert_data(cursor, data)  # Insert data into the tables
 
     connection.commit()
     connection.close()
